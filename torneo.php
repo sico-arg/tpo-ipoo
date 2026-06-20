57ba4e97415ae8251677809f8d09d3575d2643c9
sico-arg <lucianomarzoratti@gmail.com>
2026-06-20 18:49:54 -0300

    A

--- torneo.php
+++ torneo.php
@@ -1,13 +1,171 @@
 <?php
+// ==========================================
+//                 INCLUSIONES                 
+// ==========================================
+
 
 include 'personaje.php';
 include 'arma.php';
 include 'arena.php';
 include 'duelo.php';
 
-class torneo {
-    private array $personajes =[];
-    private array  $armas =[];
-    private array  $arenas =[];
-    private array $duelos =[];
+
+class torneo
+{
+
+    // ==========================================
+    //                 ATRIBUTOS              
+    // ==========================================
+    private array $personajes = [];
+    private array  $armas = [];
+    private array  $arenas = [];
+    private array $duelos = [];
+
+
+    // ==========================================
+    //                 CONSTRUCTOR         
+    // ==========================================
+    public function __construct() {}
+
+    // ==========================================
+    //                 GETTERS          
+    // ==========================================
+
+    public function getpersonajes()
+    {
+        return $this->personajes;
+    }
+    public function getarmas()
+    {
+        return $this->armas;
+    }
+    public function getarenas()
+    {
+        return $this->arenas;
+    }
+
+    public function getduelos()
+    {
+        return $this->duelos;
+    }
+
+    // ==========================================
+    //                 SETTERS              
+    // ==========================================
+
+    // ==========================================
+    //                 METODOS              
+    // ==========================================
+
+
+
+    public function agregarPersonaje(personaje $personaje)
+    {
+        $this->personajes[] = $personaje;
+    }
+    public function agregarArma(arma $arma)
+    {
+        $this->armas[] = $arma;
+    }
+    public function agregarArena(arena $arena)
+    {
+        $this->arenas[] = $arena;
+    }
+    public function equiparArma()
+    {
+        $exito = false;
+        $armaEquipada = null;
+        $personajeArma = null;
+
+        foreach ($this->personajes as $personaje) {
+            if ($personaje->getid()) {
+                $personajeArma = $personaje;
+                break;
+            }
+        }
+        foreach ($this->armas as $arma) {
+            if ($arma->getid()) {
+                $armaEquipada = $arma;
+                break;
+            }
+        }
+        if ($personajeArma !== null && $armaEquipada !== null) {
+            if ($armaEquipada->puedeSerEquipadoPor($personajeArma)) {
+                $personajeArma->setarma($armaEquipada);
+                $armaEquipada->setEstado('equipado');
+                $exito = true;
+            }
+        }
+        return $exito;
+    }
+    public function realizarDuelo()
+    {
+        $realizado = false;
+        foreach ($this->duelos as $duelo) {
+            if ($duelo->getid()) {
+                if ($duelo->x()) {
+                    //necesito un metodo de la clase duelo)
+                    $realizado = true;
+                }
+                break;
+            }
+        }
+        return $realizado;
+    }
+    public function rankingPersonaje()
+    {
+        $ranking = $this->personajes;
+        $puntaje = 0;
+        foreach ($this->duelos as $duelo) {
+            if ($ranking) {
+                $ganadorPersonaje1 =  $duelo->obtenerGanador();
+                $ganadorPersonaje2 =  $duelo->obtenerGanador();
+            }
+            if ($ganadorPersonaje1 == $ganadorPersonaje2) {
+                $ranking = 0;
+            }
+            if ($ganadorPersonaje1 > $ganadorPersonaje2) {
+                $puntaje++;
+            }
+            if ($ganadorPersonaje2 > $ganadorPersonaje1) {
+                $puntaje++;
+            }
+            $ranking = $puntaje;
+        }
+        return $ranking;
+    }
+
+    public function listarPersona()
+    {
+        $listadoPersonaje = "";
+        foreach ($this->personajes as $personaje) {
+            $listadoPersonaje = count($personaje);
+        }
+        return $listadoPersonaje;
+    }
+    public function listarArma()
+    {
+        $listadoArma = "";
+        foreach ($this->armas as $arma) {
+            $listadoArma = count($arma);
+        }
+        return $listadoArma;
+    }
+    public function listarArena()
+    {
+        $listadoArena = "";
+        foreach ($this->arenas as $arena) {
+            $listadoArena = count($arena);
+        }
+        return $listadoArena;
+    }
+
+    public function listarDuelos()
+    {
+        $listadoDuelos = "";
+        foreach ($this->duelos as $duelo) {
+            $listadoDuelos = count($duelo);
+        }
+        return $listadoDuelos;
+    }
 }
